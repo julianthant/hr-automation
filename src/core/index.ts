cdbcd143ab01fef451986bc9116e2916c4f933f8
julianthant <julianthant@gmail.com>
2026-04-15 11:19:37 -0700

    feat(core): export full public API from barrel

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,6 @@
 export type * from './types.js'
 export { register, getAll, getByName, clear } from './registry.js'
-export { defineWorkflow } from './workflow.js'
+export { defineWorkflow, runWorkflow, runWorkflowBatch } from './workflow.js'
+export { runWorkflowPool } from './pool.js'
+export { Session } from './session.js'
+export { Stepper } from './stepper.js'
